0eeeb70ce9bfa67baa13f1137c215b57505b3dd5
philipp-baumann <spectral-cockpit@proton.me>
2023-02-10 21:26:57 +0100

    figure out error In BFS for particular data set

--- _reprex_BFS_metdata.R
+++ _reprex_BFS_metdata.R
@@ -35,4 +35,25 @@ meta_social_welfare <- get_pxweb_meta_social_welfare()
 
 str(meta_pxweb_social_welfare$variables)
 
+# do some manual debugging
+language <- "de"
+number_bfs <- "px-x-1304030000_121"
+url_bfs <- "https://www.bfs.admin.ch/bfs/de/home/statistiken/soziale-sicherheit/sozialhilfe/sozialhilfebeziehende/wirtschaftliche-sozialhilfe.assetdetail.23605870.html"
+
+html_raw <- xml2::read_html(url_bfs)
+html_table <- rvest::html_node(html_raw, ".table")
+df_table <- rvest::html_table(html_table)
+number_bfs <- df_table$X2[grepl("px", df_table$X2)]
+
+pxweb_api_url <- paste0("https://www.pxweb.bfs.admin.ch/api/v1/", language, "/", number_bfs, "/", number_bfs, ".px")
+df_json <- jsonlite::fromJSON(txt = pxweb_api_url)
+
+variables <- df_json$variables$code
+values <- df_json$variables$values
+# why this is hard-coded? is this given by by the PxWeb spec?
+df <- rbind(c("*", "*","*","*"))
+names(df) <- variables
+dims <- as.list(df)
+pxq <- pxweb::pxweb_query(dims)
+
 sessioninfo::session_info()
